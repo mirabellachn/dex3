c8b62f57444a7218c24e7acf046df1adf5573b5e
mirabellachn <mirabellacn@gmail.com>
2025-02-03 16:16:23 +0700

    design pokedex row

---
+++ Dex3/PokemonDetail.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonDetail.swift
+//  Dex3
+//
+//  Created by mirabella  on 01/02/25.
+//
+
+import SwiftUI
+
+struct PokemonDetail: View {
+    @EnvironmentObject var pokemon: Pokemon
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonDetail()
+}
